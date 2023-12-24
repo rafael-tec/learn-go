efe52e24410eadf82a59736002b554429315a00f
Rafael Martins <rafaeltecinfo9@gmail.com>
2023-12-24 12:39:39 -0300

    More examples using context

---
+++ import_packages/http_server_context/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	log.Println("Started request")
+	defer log.Println("Finished request")
+
+	select {
+	case <-time.After(5 * time.Second):
+		log.Println("Request processed successfully")
+		w.Write([]byte("Request processed successfully"))
+	case <-ctx.Done():
+		log.Println("Request cancelled by client")
+	}
+}
